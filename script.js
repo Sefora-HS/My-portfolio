ff3b916b5426819bb4388644016cefe815543cad
Sefora-HS <sefora.sylla@hotmail.com>
2025-02-20 20:14:39 +0100

    add sections + contact button with js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 document.getElementById("myButton").onclick = function() {
     document.getElementById("projects").scrollIntoView({ behavior: 'smooth' });
 };
+
+document.getElementById("contact-button").onclick = function() {
+    document.getElementById("contact").scrollIntoView({ behavior: 'smooth' });
+};
